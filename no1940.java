e91d5575d79c4bb53ca2a447fdaab3d203ef3592
parkhyunseo <dazzle1999@naver.com>
2023-09-19 21:52:42 +0900

    No.1940: 주몽

---
+++ no1940.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class no1940 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[N];
+        for(int i =0; i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+
+        int sum=0;
+        int count=0;
+        int first = 0;
+        int last = N-1;
+
+        while(first<last){
+            if(arr[first]+arr[last]>M){
+                last--;
+            } else if(arr[first]+arr[last] == M){
+                count++;
+                first++;
+                last--;
+            }else{
+                first++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
