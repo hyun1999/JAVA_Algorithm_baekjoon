9a00a855650d4e741e3a289f0171e8663e40b111
parkhyunseo <dazzle1999@naver.com>
2023-09-24 01:36:46 +0900

    No.1543: 문서검색

--- 문자열/no1543.java
+++ 문자열/no1543.java
@@ -2,23 +2,14 @@ package 문자열;
 
 import java.util.Scanner;
 
-public class no1545 {
+public class no1543 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String doc = scan.nextLine();
         String word = scan.nextLine();
 
-        int count = 0;
-        int startIndex = 0;
-
-        while(true){
-            int findIndex = doc.indexOf(word,startIndex);
-            if(findIndex<0)
-                break;
-            count++;
-            startIndex = findIndex+word.length();
-
-        }
+        String replaced = doc.replaceAll(word,"");
+        int count = (doc.length() - replaced.length())/word.length();
         System.out.println(count);
     }
 }
