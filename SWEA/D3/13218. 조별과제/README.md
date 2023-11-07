52d33c51985390b0bb1522e9d85eff6b122ab364
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-07 15:32:01 +0900

    [D3] Title: 조별과제, Time: 170 ms, Memory: 24,972 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/README.md
@@ -0,0 +1,15 @@
+# [D3] 조별과제 - 13218 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXzjvCCq-PwDFASs) 
+
+### 성능 요약
+
+메모리: 24,972 KB, 시간: 170 ms, 코드길이: 328 Bytes
+
+### 제출 일자
+
+2023-11-07 15:31
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/13218. 조별과제/조별과제.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            int value = scan.nextInt();
+            System.out.println("#"+tc+" "+value/3);
+        }
+    }
+}
