cc5c4630a902219aa8bfa2eee107bbeda00d3697
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-13 20:59:13 +0900

    [D3] Title: 동철이의 프로그래밍 대회, Time: 161 ms, Memory: 28,940 KB -BaekjoonHub

---
+++ SWEA/D3/6958. 동철이의 프로그래밍 대회/동철이의 프로그래밍 대회.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = scan.nextInt(); // N = 사람 수
+            int M = scan.nextInt(); // M = 푼 문제 수
+            int maxProblem = 0;
+            int maxPeople = 0;
+            for (int i = 0; i < N; i++) {
+                int totalProblem = 0;
+                for (int j = 0; j < M; j++) {
+                    totalProblem += scan.nextInt();
+                }
+                if(totalProblem>maxProblem){
+                    maxProblem = totalProblem;
+                    maxPeople = 0;
+                }
+                if(totalProblem == maxProblem){
+                    maxPeople++;
+                }
+            }
+
+            System.out.println("#"+tc+" "+maxPeople+" "+maxProblem);
+
+
+        }
+    }
+}
