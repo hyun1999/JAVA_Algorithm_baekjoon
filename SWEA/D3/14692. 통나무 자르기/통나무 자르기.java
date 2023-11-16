64db19bd05f44b68fe835e7a85d300b6f4fbc1ab
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-16 16:57:44 +0900

    [D3] Title: 통나무 자르기, Time: 141 ms, Memory: 20,892 KB -BaekjoonHub

---
+++ SWEA/D3/14692. 통나무 자르기/통나무 자르기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        for (int tc = 1; tc <=T; tc++) {
+            int value = scan.nextInt();
+            if(value%2 == 1){
+                System.out.println("#"+tc+" Bob");
+            }else{
+                System.out.println("#"+tc+" Alice");
+            }
+        }
+    }
+}
