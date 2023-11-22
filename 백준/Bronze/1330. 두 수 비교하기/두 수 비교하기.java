38af563c1670f0af6273dfe1817ac8fc63629368
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-22 17:19:45 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        String c = (a>b) ? ">":((a<b) ? "<":"==");
+        System.out.println(c);
+
+    }
+}
