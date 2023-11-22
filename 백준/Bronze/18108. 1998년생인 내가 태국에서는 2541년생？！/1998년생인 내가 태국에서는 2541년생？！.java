0bd9c15d67d10d13370b2f47af164069f89a56a4
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-22 16:44:19 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        System.out.println(a-543);
+    }
+}
