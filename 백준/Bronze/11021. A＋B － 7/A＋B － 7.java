74430132fc12e46e709425d5ab47feb74aa461cc
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-22 19:07:23 +0900

    [Bronze V] Title: A+B - 7, Time: 276 ms, Memory: 18692 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for (int i = 0; i < N; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println("Case #"+(i+1)+": "+(a+b));
+        }
+    }
+}
