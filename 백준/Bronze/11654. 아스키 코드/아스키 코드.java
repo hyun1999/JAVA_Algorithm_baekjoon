7126ede291920a5f311525eb66c576e23af362b6
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-23 16:30:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String ch = scan.next();
+        System.out.println(ch.charAt(0)-'0'+'0');
+    }
+}
