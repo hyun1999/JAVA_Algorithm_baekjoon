ca325ff1edcd95e866084ea00ba9b0de7d87c92a
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-27 02:11:59 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 612 ms, Memory: 36400 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+        int[][] arr = new int[N][M];
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    arr[i][j]+=scan.nextInt();
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
