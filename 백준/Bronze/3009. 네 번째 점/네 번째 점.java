6ab85d7c140bce0ace1d7791f91525e7baeafd94
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-29 21:08:06 +0900

    [Bronze III] Title: 네 번째 점, Time: 220 ms, Memory: 18164 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int[] x = new int[3];
+        int[] y = new int[3];
+        for (int i = 0; i < 3; i++) {
+            x[i] = scan.nextInt();
+            y[i] = scan.nextInt();
+        }
+        if(x[0] == x[1]){
+            System.out.print(x[2]+" ");
+        }else if(x[1]==x[2]){
+            System.out.print(x[0]+" ");
+        }else{
+            System.out.print(x[1]+" ");
+        }
+
+        if(y[0] == y[1]){
+            System.out.print(y[2]);
+        }else if(y[1]==y[2]){
+            System.out.print(y[0]);
+        }else{
+            System.out.print(y[1]);
+        }
+    }
+}
