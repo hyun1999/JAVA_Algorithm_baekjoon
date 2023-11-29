97af7b8fead2fb861d07941a986195aa489a2a6d
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-29 21:31:18 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 208 ms, Memory: 17416 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        if(a+b+c == 180){
+            if(a==b&&b==c&&a==c){
+                System.out.println("Equilateral");
+            }else if(a==b||b==c||a==c){
+                System.out.println("Isosceles");
+            }else{
+                System.out.println("Scalene");
+            }
+        }else{
+            System.out.println("Error");
+        }
+    }
+}
