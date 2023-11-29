ef9ab7b73e840c5d340ad97393112bd6e56d6749
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-11-29 22:11:03 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        Long N = scan.nextLong();
+        System.out.println((N-1)*N/2);
+        System.out.println('2');
+    }
+}
