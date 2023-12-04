d2816315d36e4c47da4e9f868193e97529dd8ace
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-12-04 23:37:42 +0900

    [Silver II] Title: 랜선 자르기, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,43 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static boolean isPossible(int[] arr, long mid, int N){
-        long sum = 0;
-        for(int a : arr){
-            sum += a/mid;
-        }
-        return sum >= N;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int K = Integer.parseInt(st.nextToken()); // 랜선 갯수
+        int N = Integer.parseInt(st.nextToken()); // 만들고싶은 갯수
 
         int[] arr = new int[K];
+        long max = 0;
+        long min = 1;
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
             arr[i] = Integer.parseInt(st.nextToken());
-        }
-        long ans = -1;
-        long l = 1;
-        long r = (1L << 31)-1;
-
-        while(l<=r){
-            long mid = (l+r)/2;
-            if(isPossible(arr,mid,N)){
-                ans = mid;
-                l = mid+1;
-            }else{
-                r = mid-1;
+            if(max<arr[i]){
+                max = arr[i];
             }
         }
-        System.out.println(ans);
+        long mid = 0;
+        while(min<=max){
+            mid = (min+max)/2;
+            int count = 0;
+            for (int i = 0; i < K; i++) {
+                count+=arr[i]/mid;
+            }
+            if(count<N){
+                max = mid-1;
+            }else{
+                min = mid+1;
+            }
+
+        }
+        System.out.println(min-1);
     }
 }
