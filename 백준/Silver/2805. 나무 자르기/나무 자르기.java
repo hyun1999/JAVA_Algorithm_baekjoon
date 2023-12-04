e681047fecf14d41384ca76ee18c88752b2936f2
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2023-12-04 23:14:57 +0900

    [Silver II] Title: 나무 자르기, Time: 540 ms, Memory: 119196 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,49 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static boolean isPossible(int[] height, int cutH, int M){
-        long sum = 0;
-        for(int h: height){
-            if(h>cutH) {
-                sum += h-cutH;
-            }
-        }
-        return sum >= M;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(st.nextToken()); // 나무갯수
+        int M = Integer.parseInt(st.nextToken()); // 가져가려고 하는 나무의 길이
 
+        int[] arr = new int[N];
+        int min = 0;
+        int max = 0;
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        // 1. 절단기 높이의 탐색 범위를 정한다.
-        int l = 0, r = 1000000000, ans = -1;
-
-        // 2. 임의의 절단기 높이에 대해
-        // 2-1. 원하는 만큼 나무를 가져갈 수 있다면 높이를 높여본다.
-        // 2-2. 원하는 나무만큼 가져갈 수 없다면 높이를 낮춰본다.
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(isPossible(arr,mid,M)){
-                ans = mid;
-                l = mid+1;
-            }
-            else{
-                r = mid-1;
+            if(arr[i]>max){
+                max = arr[i];
             }
         }
-        // 3. 원하는 만큼 가져갈 수 있었던 높이 중 최댓값을 출력한다.
-        System.out.println(ans);
+
+        int mid = 0;
+        while(min<max){
+            long total = 0;
+            mid = (min+max)/2;
+
+            for (int i = 0; i < N; i++) {
+                if(arr[i]-mid>0){
+                    total+=arr[i]-mid;
+                }
+            }
+            if(total<M){
+                max = mid;
+            }else{
+                min = mid+1;
+            }
+        }
+        System.out.println(min-1);
     }
 }
