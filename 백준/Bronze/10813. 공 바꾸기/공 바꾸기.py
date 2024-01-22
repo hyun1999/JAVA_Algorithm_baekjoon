0602119019485e0b1d73207c529dbd51fe10fcfd
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-01-23 00:12:26 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,9 +1,16 @@
 M,N = map(int,input().split())
-array = [i for i in range(1,M+1)]
+array = []
 
+def change(first,last):
+    tmp = array[first-1]
+    array[first-1] = array[last-1]
+    array[last-1] = tmp
+
+for i in range(1,M+1):
+    array.append(i)
 for i in range(N):
     first, last = map(int,input().split())
-    array[first-1],array[last-1] = array[last-1],array[first-1]
+    change(first,last)
 
 for i in range(M):
     print(array[i],end=' ')
