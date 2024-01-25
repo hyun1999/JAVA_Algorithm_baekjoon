d1e8af1c01f8ec1533a4ee328130cab22ad7e523
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-01-26 00:57:03 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,11 +1,7 @@
 n = int(input())
 for _ in range(n):
     money = int(input())
-    print(money//25,end=' ')
-    money %= 25
-    print(money//10,end=' ')
-    money %= 10
-    print(money//5,end=' ')
-    money %= 5
-    print(money,end=' ')
+    for i in [25,10,5,1]:
+        print(money//i,end=' ')
+        money%=i
     print()
