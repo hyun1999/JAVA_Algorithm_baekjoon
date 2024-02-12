ad52300ba1cb1cc7637b89b2c5a9661cc3b5e7a0
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-02-13 00:29:04 +0900

    [Silver V] Title: 나이순 정렬, Time: 4176 ms, Memory: 62524 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -3,8 +3,10 @@ arr = []
 for i in range(n):
     age,name = map(str,input().split())
     age = int(age)
-    arr.append((age,name))
+    arr.append([age,name])
+for i in range(n):
+    arr[i].append(i)
 
-arr.sort(key = lambda x:(x[0]))
+arr.sort(key = lambda x:(x[0],x[2]))
 for i in arr:
     print(i[0],i[1])
