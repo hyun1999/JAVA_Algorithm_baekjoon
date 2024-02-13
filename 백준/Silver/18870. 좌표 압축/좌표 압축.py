1b8a63e7afc9dee226cf420a25d82893b0463fac
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-02-13 18:02:17 +0900

    [Silver II] Title: 좌표 압축, Time: 1900 ms, Memory: 144340 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,21 +1,9 @@
-n = int(input())
-arr = list(map(int,input().split()))
-arrCopy = []
+n=int(input())
+a=list(map(int,input().split()))
+sa=sorted(set(a)) # set을 이용한 중복 제거 및 정렬
 
-for i in range(n):
-    arrCopy.append([arr[i],i])
-
-arrCopy.sort()
-arrCopy2 = []
-count = 0
-arrCopy2.append([arrCopy[0],0])
-for i in range(1,len(arrCopy)):
-    if arrCopy[i-1][0]==arrCopy[i][0]:
-        arrCopy2.append([arrCopy[i],count])
-    else :
-        count+=1
-        arrCopy2.append([arrCopy[i],count])
-arrCopy2.sort(key = lambda x:(x[0][1]))
-
-for i in range(n):
-    print(arrCopy2[i][1],end=' ')
+dic={}
+for i in range(len(sa)):
+    dic[sa[i]]=i #인덱스 사전 만들기
+for x in a:
+    print(dic[x],end=' ')
