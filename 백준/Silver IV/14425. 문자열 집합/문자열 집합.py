9a621f20f28ea43198b067d7fe3828865917ee46
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-02-14 15:49:52 +0900

    [Silver IV] Title: 문자열 집합, Time: 116 ms, Memory: 36368 KB -BaekjoonHub

--- 백준/Silver IV/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver IV/14425. 문자열 집합/문자열 집합.py
@@ -1,10 +1,10 @@
 import sys
 n,m = map(int,sys.stdin.readline().split())
-list_s = []
+set_s = {}
 for _ in range(n):
-    list_s.append(sys.stdin.readline())
+    set_s[sys.stdin.readline()] = True
 result = 0
 for _ in range(m):
-    if sys.stdin.readline() in list_s:
+    if sys.stdin.readline() in set_s:
         result+=1
 print(result)
