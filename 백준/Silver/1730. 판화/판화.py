0d15b9a13d214802d5ee65247af103759a53c76e
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-03-03 20:33:54 +0900

    [Silver IV] Title: 판화, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1730. 판화/판화.py
+++ 백준/Silver/1730. 판화/판화.py
@@ -1,26 +1,26 @@
 n = int(input())
 row_map = [[False for col in range(n)] for row in range(n)]
 col_map = [[False for col in range(n)] for row in range(n)]
-cur_x = 0
 cur_y = 0
+cur_x = 0
 input_str = input()
 for i in input_str:
-    if i == 'D' and cur_x<n-1:
-        col_map[cur_x][cur_y] = True
-        col_map[cur_x+1][cur_y] = True
-        cur_x+=1
-    elif i == 'R' and cur_y<n-1:
-        row_map[cur_x][cur_y] = True
-        row_map[cur_x][cur_y+1] = True
+    if i == 'D' and cur_y<n-1:
+        col_map[cur_y][cur_x] = True
+        col_map[cur_y+1][cur_x] = True
         cur_y+=1
-    elif i == 'U' and cur_x>0:
-        col_map[cur_x][cur_y] = True
-        col_map[cur_x-1][cur_y] = True
-        cur_x-=1
-    elif i == 'L'and cur_y>0:
-        row_map[cur_x][cur_y] = True
-        row_map[cur_x][cur_y-1] = True
+    elif i == 'R' and cur_x<n-1:
+        row_map[cur_y][cur_x] = True
+        row_map[cur_y][cur_x+1] = True
+        cur_x+=1
+    elif i == 'U' and cur_y>0:
+        col_map[cur_y][cur_x] = True
+        col_map[cur_y-1][cur_x] = True
         cur_y-=1
+    elif i == 'L'and cur_x>0:
+        row_map[cur_y][cur_x] = True
+        row_map[cur_y][cur_x-1] = True
+        cur_x-=1
     else:
         continue
 
