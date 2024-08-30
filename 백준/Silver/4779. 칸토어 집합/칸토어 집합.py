9370a3c71b3ebccc838f37e0c2ecbd10333768e2
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-08-31 01:58:45 +0900

    [Silver III] Title: 칸토어 집합, Time: 112 ms, Memory: 111244 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,26 +1,16 @@
-import sys
+def func(k):
+	if k == 0:
+		print('-',end="")
+		return
 
-def recursion(arr,start,length):
-	if length<1:
-		return arr
+	func(k-1)
+	print(' '*(3**(k-1)),end="")
+	func(k-1)
 
-	mid_start = start+length
-	mid_end = start+2*length
-	arr[mid_start:mid_end] = [''] * length
-	recursion(arr, start, length//3)
-	recursion(arr, mid_end,length//3)
-	
-while(1):
-	val = sys.stdin.readline().rstrip()
-	if val == "":
+while True:
+	try:
+		N = int(input())
+		func(N)
+		print()
+	except:
 		break
-	val = int(val)
-	arr = ['-']*(3**val)
-	length = 3**val // 3
-	recursion(arr,0,length)
-	for i in arr:
-		if i == '-':
-			print('-',end="")
-		else:
-			print(' ',end="") 
-	print()
