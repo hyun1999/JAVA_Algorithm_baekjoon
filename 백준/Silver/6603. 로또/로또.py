a20c6984eeb4ae727d79d9d2b4d9051d85c1b402
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-08-31 16:59:00 +0900

    [Silver II] Title: 로또, Time: 104 ms, Memory: 110420 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,22 +1,13 @@
+from itertools import combinations
 import sys
-a = []
-k = 6
-
-def recur(index,s):
-	if len(a)==k:
-		print(" ".join(map(str,a)))
-		return
-
-	for i in range(index, len(s)):
-		a.append(s[i])
-		recur(i+1,s)
-		a.pop()
 
 while True:
-	s = list(map(int,sys.stdin.readline().split(' ')))
-	if s[0] == 0:
+	a = list(map(int,sys.stdin.readline().split(' ')))
+	if a[0] == 0:
 		break
-	s.pop(0)
-	recur(0,s)
-	print()
 
+	s = a[1:]
+	comb = list(combinations(s,6))
+	for i in comb:
+		print(" ".join(map(str,i)))
+	print()
