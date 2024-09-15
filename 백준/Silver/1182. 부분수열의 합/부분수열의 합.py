d86248c4d0ce6d0ebd5aae341a567b21b6fbedf9
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-09-15 15:06:14 +0900

    [Silver II] Title: 부분수열의 합, Time: 276 ms, Memory: 113332 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,20 @@
+def search(lev):
+	global N,S,arr,choose,ans
+
+	# base case
+	if lev == N:
+		if choose and sum(choose) == S:
+			ans +=1
+		return
+	choose.append(arr[lev])
+	search(lev+1)
+	choose.pop()
+
+	search(lev+1)
+
+N,S = map(int,input().split(" "))
+arr = list(map(int,input().split(" ")))
+choose = []
+ans = 0
+search(0)
+print(ans)
