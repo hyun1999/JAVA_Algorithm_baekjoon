9ff261bfb315db1e4380c4c5dae3f251b3cb2aea
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2024-09-19 18:48:50 +0900

    [Gold IV] Title: 도서관, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -1,60 +1,23 @@
-N, M = map(int,input().split(" "))
-books = list(map(int,input().split(" ")))
-books = sorted(books)
+N,M = map(int,input().split(" "))
+arr = list(map(int,input().split(" ")))
 
-plus_books=[]
-minus_books=[]
-far_val = 0
-most_far_val = 0
-total_walk = 0
-def func():
-	global plus_books, minus_books, far_val, most_far_val, total_walk
-	for i in books:
-		if i>=0:
-			plus_books.append(i)
-			if abs(i) > abs(far_val):
-				most_far_val = i
-				far_val = abs(i)
-		else:
-			minus_books.append(i)
-			if abs(i) > abs(far_val):
-				most_far_val = i
-				far_val = abs(i)
+neg = []
+pos = []
 
-	# 음수이면
-	if most_far_val < 0:
-		total_walk += abs(minus_books.pop(0))
-		for _ in range(min(M-1,len(minus_books))):
-			minus_books.pop(0)
-	# 양수이면
+for x in arr:
+	if x>0:
+		pos.append(x)
 	else:
-		total_walk += abs(plus_books.pop(len(plus_books)-1))
-		for _ in range(min(M-1,len(plus_books))):
-			plus_books.pop(len(plus_books)-1)
-	# 초기 세팅 완료
+		neg.append(-x)
 
-	# 양수
-	while len(plus_books) > 0:
-		try:
-			total_walk += 2*plus_books.pop()
-			for _ in range(min(M-1,len(plus_books))):
-				plus_books.pop()
-		except:
-			break
-	# 음수
-	while len(minus_books) > 0:
-		try:
-			val = abs(minus_books[0])
-			total_walk += 2*abs(minus_books.pop(0))
-			for _ in range(min(M-1,len(minus_books))):
-				minus_books.pop(0)
-		except:
-			break
-if len(books) == 1 :
-	print(books[0])
-elif len(books) == 0:
-	print(0)
-else:
-	func()
-	print(total_walk)
+pos = sorted(pos)[::-1]
+neg = sorted(neg)[::-1]
 
+dists = []
+
+for p in pos[::M]:
+	dists.append(p)
+for q in neg[::M]:
+	dists.append(q)
+
+print(2*sum(dists) - max(dists))
