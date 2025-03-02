e6d042b4e373f15976fca2357f7d5f7b6fb94144
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2025-03-02 21:39:32 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13956 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,8 +1,15 @@
-class Main {
-    public static void main(String[] args){
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+public static void main(String[] args) throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    bw.write("\\    /\\\n");
+    bw.write(" )  ( ')\n");
+    bw.write("(  /  )\n");
+    bw.write(" \\(__)|\n");
+    bw.flush();
+    bw.close();
     }
 }
