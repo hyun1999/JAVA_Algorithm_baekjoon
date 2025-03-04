0bafd826c9b045a041302f9d6e389bd2d51c3d3a
ParkHyunSeo <74495717+hyun1999@users.noreply.github.com>
2025-03-04 23:07:29 +0900

    [Bronze I] Title: 단어 공부, Time: 480 ms, Memory: 34508 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,41 +1,24 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
 
-        String str = scan.next();
-        int[] arr = new int[27];
-        str = str.toUpperCase();
-        for (int i = 0; i < str.length(); i++) {
-            arr[str.charAt(i)-'A']++ ;
-        }
-        // str의 값 적절한 index에 입력 완료
-        int max = 0;
-        int index = 0;
-        for (int i = 0; i <= 26; i++) {
-            if(arr[i]>max){
-                max = arr[i];
-                index = i;
+        Scanner scan =  new Scanner(System.in);
+        char[] str = scan.nextLine().toUpperCase().toCharArray();
+        int[] alpha = new int[26];
+        for(int i=0; i<str.length; i++) alpha[str[i] - 'A']++;
+        int maxVal = -1;
+        char ch = '?';
+        for (int i = 0; i < 26; i++) {
+            if(alpha[i] > maxVal) {
+                maxVal = alpha[i];
+                ch = (char)(i+65);
+            }else if(alpha[i] == maxVal){
+                ch = '?';
             }
         }
-        // 최댓값 찾기
+        System.out.println(ch);
 
-        int count = 0;
-        for (int i = 0; i <= 26; i++) {
-            if(max==arr[i]){
-                count++;
-            }
-        }
-        // 최댓값이 2개일 경우
-
-        if(count >= 2){
-            System.out.println("?");
-        }else{
-            int index2 = index+'A';
-            char index3 = (char) index2;
-            System.out.println(index3);
-        }
     }
 }
